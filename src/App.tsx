b47297982319c572e13d481813421bb9ec4b1c6e
DGwebdes <alan.bilik8@gmail.com>
2026-01-29 20:30:32 +0000

    we are linked. pushes to this repo trigger gh actions, it builds the app, then pushes to cp-deploy repo. exp-99

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 function App() {
+    const [count, setCount] = useState(0);
     return (
         <>
             <h1>cPanel + Github workflow</h1>
+            <p>A small update to test</p>
+            <button onClick={() => setCount(count + 1)}>{count}</button>
         </>
     );
 }
